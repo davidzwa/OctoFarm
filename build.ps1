b8982521035a9efc5b8fc5a001e9c809704ca66b
David Zwart <davidzwa@gmail.com>
2024-12-06 23:53:54 +0100

    build and release

--- build.ps1
+++ build.ps1
@@ -1,1 +1,1 @@
-docker build . -f .\docker\Dockerfile -t fdmmonster/octofarm:1.8.0-beta
+docker build . -f .\docker\Dockerfile -t fdmmonster/octofarm:1.8.0-beta-ui-refactor
--- build_push.ps1
+++ build_push.ps1
@@ -1,2 +1,1 @@
-docker build . -f .\docker\Dockerfile -t fdmmonster/octofarm:1.7.4 --push
-docker build . -f .\docker\Dockerfile -t fdmmonster/octofarm:1.8.0-beta --push
+docker build . -f .\docker\Dockerfile -t fdmmonster/octofarm:1.8.0-beta-ui-refactor --push
